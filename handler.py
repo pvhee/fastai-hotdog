a5c2a18f11e7f9a7ed69d120944e03ff9f682d8f
pvhee <peter.vanhee@gmail.com>
2019-04-30 22:37:10 +0100

    Enabling CORS

--- handler.py
+++ handler.py
@@ -165,5 +165,9 @@ def lambda_handler(event, context):
     print("Returning response")
     return {
         "statusCode": 200,
+        "headers": {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        },
         "body": json.dumps(response)
     }
